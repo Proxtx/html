e50c340ed4fb0d5cd5546e29d8bc7538285ee986
Proxtx <39201896+Proxtx@users.noreply.github.com>
2022-04-06 19:56:18 +0200

    moved the entire project into a class but exporting the exports from a class instance created within the parser

--- parser.js
+++ parser.js
@@ -1,204 +1,182 @@
-const genCharArray = (string) => {
-  let saveObj = {};
-  let escapedString = "";
-  let stringSplit = string.split("");
-  let charArray = [];
-  let escaped = false;
-  for (let i in stringSplit) {
-    if (stringSplit[i] == "\\" && !escaped) {
-      escaped = true;
-      continue;
+export class Parser {
+  /**
+   * Parses html source code into arrays strings and objects (json)
+   * @param {String} html html source code
+   * @returns Parsed html code
+   */
+  parse = (html) => {
+    return this.loop(new Utility(html));
+  };
+
+  loop = (utility) => {
+    const resultArray = [];
+    for (let i in utility.charArray) {
+      let result = this.step(utility);
+      if (result) resultArray.push((utility.prevElem = result));
+      try {
+        utility.next();
+      } catch (e) {
+        break;
+      }
     }
-    let obj;
-    let position = escapedString.length;
-    charArray.push(
-      (obj = {
-        char: stringSplit[i],
-        escaped,
-        position,
-        getString: function () {
-          return this.saveObj.string.substring(this.position);
-        }.bind({ saveObj, position }),
-      })
-    );
-    escapedString += stringSplit[i];
-    escaped = false;
-  }
-  saveObj.string = escapedString;
-  return charArray;
-};
+    return resultArray;
+  };
 
-/**
- * Parses html source code into arrays strings and objects (json)
- * @param {String} html html source code
- * @returns Parsed html code
- */
-export const parse = (html) => {
-  return loop(new Utility(html));
-};
+  regExpLib = {
+    htmlTag: /.*?(>|\s)/,
+    htmlStart: /</,
+    htmlEnd: />/,
+    attributeStart: /\w/,
+    attributeType: /.*?(\=|\s|\>)/,
+    attributeValueStart: /\"/,
+    attributeValueEnd: /\"/,
+    attributeValueExist: /((?=\S)|(?=\>))(?=[^\=])/,
+    attributeValueEqual: /\=/,
+    htmlLoopEnd: /^<\//,
+    htmlLoopTagEnd: />/,
+    htmlCommentStart: /^!--/,
+    htmlCommentEnd: /^-->/,
+  };
 
-const loop = (utility) => {
-  const resultArray = [];
-  for (let i in utility.charArray) {
-    let result = step(utility);
-    if (result) resultArray.push((utility.prevElem = result));
-    try {
+  selfClosingTags = [
+    "area",
+    "base",
+    "br",
+    "col",
+    "embed",
+    "hr",
+    "img",
+    "input",
+    "link",
+    "meta",
+    "param",
+    "source",
+    "track",
+    "wbr",
+    "command",
+    "keygen",
+    "menuitem",
+    "!doctype",
+  ];
+
+  parsers = {
+    html: (utility) => {
+      if (
+        !utility.char.char.match(this.regExpLib.htmlStart) ||
+        utility.char.escaped
+      )
+        return;
+      let obj = { type: "html" };
+      utility.prevElem = obj;
       utility.next();
-    } catch (e) {
-      break;
-    }
-  }
-  return resultArray;
-};
-
-const regExpLib = {
-  htmlTag: /.*?(>|\s)/,
-  htmlStart: /</,
-  htmlEnd: />/,
-  attributeStart: /\w/,
-  attributeType: /.*?(\=|\s|\>)/,
-  attributeValueStart: /\"/,
-  attributeValueEnd: /\"/,
-  attributeValueExist: /((?=\S)|(?=\>))(?=[^\=])/,
-  attributeValueEqual: /\=/,
-  htmlLoopEnd: /^<\//,
-  htmlLoopTagEnd: />/,
-  htmlCommentStart: /^!--/,
-  htmlCommentEnd: /^-->/,
-};
-
-const selfClosingTags = [
-  "area",
-  "base",
-  "br",
-  "col",
-  "embed",
-  "hr",
-  "img",
-  "input",
-  "link",
-  "meta",
-  "param",
-  "source",
-  "track",
-  "wbr",
-  "command",
-  "keygen",
-  "menuitem",
-  "!doctype",
-];
-
-export const parsers = {
-  html: (utility) => {
-    if (!utility.char.char.match(regExpLib.htmlStart) || utility.char.escaped)
-      return;
-    let obj = { type: "html" };
-    utility.prevElem = obj;
-    utility.next();
-    let tag = utility.char.getString().match(regExpLib.htmlTag)[0];
-    tag = tag.substring(0, tag.length - 1);
-    if (tag.match(regExpLib.htmlCommentStart)) {
+      let tag = utility.char.getString().match(this.regExpLib.htmlTag)[0];
+      tag = tag.substring(0, tag.length - 1);
+      if (tag.match(this.regExpLib.htmlCommentStart)) {
+        utility.do((utility) => {
+          let find = utility.char.getString().match(regExpLib.htmlCommentEnd);
+          if (!find) {
+            utility.next();
+            return;
+          }
+          utility.next(find[0].length - 1);
+          return true;
+        });
+        return false;
+      }
+      utility.next(tag.length);
+      obj.tag = tag;
+      let attributes = [];
       utility.do((utility) => {
-        let find = utility.char.getString().match(regExpLib.htmlCommentEnd);
-        if (!find) {
-          utility.next();
-          return;
-        }
-        utility.next(find[0].length - 1);
-        return true;
+        if (utility.char.char.match(this.regExpLib.htmlEnd)) return true;
+        utility.next();
+        const attribute = this.parseAttribute(utility);
+        attribute && attributes.push(attribute);
       });
-      return false;
-    }
-    utility.next(tag.length);
-    obj.tag = tag;
-    let attributes = [];
-    utility.do((utility) => {
-      if (utility.char.char.match(regExpLib.htmlEnd)) return true;
+      obj.attributes = attributes;
+      if (this.selfClosingTags.includes(tag.toLowerCase())) {
+        obj.selfClosing = true;
+        return obj;
+      }
       utility.next();
-      const attribute = parseAttribute(utility);
-      attribute && attributes.push(attribute);
+      const innerHTML = [];
+      utility.do((utility) => {
+        if (
+          utility.char.getString().match(this.regExpLib.htmlLoopEnd) &&
+          !utility.char.escaped
+        )
+          return true;
+        let result = this.step(utility);
+        if (result) innerHTML.push((utility.prevElem = result));
+        utility.next();
+      });
+      obj.innerHTML = innerHTML;
+      utility.waitUntil(this.regExpLib.htmlLoopTagEnd);
+      return obj;
+    },
+    text: (utility) => {
+      if (utility.prevElem && utility.prevElem.type == "text") {
+        utility.prevElem.text += utility.char.char;
+      } else {
+        return { type: "text", text: utility.char.char };
+      }
+    },
+  };
+
+  parseAttribute = (utility) => {
+    if (!utility.char.char.match(this.regExpLib.attributeStart)) return;
+    utility.prevElem = undefined;
+    let attribute = utility.char
+      .getString()
+      .match(this.regExpLib.attributeType)[0];
+    attribute = attribute.substring(0, attribute.length - 1);
+    utility.next(attribute.length - 1);
+    let obj = { attribute, value: "" };
+    let noValue = false;
+    utility.do((utility) => {
+      utility.next();
+      if (utility.char.char.match(this.regExpLib.attributeValueExist)) {
+        noValue = true;
+        return true;
+      }
+      if (utility.char.char.match(this.regExpLib.attributeValueEqual))
+        return true;
+      return false;
     });
-    obj.attributes = attributes;
-    if (selfClosingTags.includes(tag.toLowerCase())) {
-      obj.selfClosing = true;
+    if (noValue) {
+      utility.next(-1);
       return obj;
     }
-    utility.next();
-    const innerHTML = [];
+    utility.waitUntil(this.regExpLib.attributeValueStart, true, true);
+    let value = [];
     utility.do((utility) => {
       if (
-        utility.char.getString().match(regExpLib.htmlLoopEnd) &&
+        utility.char.char.match(this.regExpLib.attributeValueEnd) &&
         !utility.char.escaped
       )
         return true;
-      let result = step(utility);
-      if (result) innerHTML.push((utility.prevElem = result));
+      let result = this.step(utility, [this.parsers.html]);
+      if (result) {
+        value.push(result);
+        utility.prevElem = result;
+      }
       utility.next();
     });
-    obj.innerHTML = innerHTML;
-    utility.waitUntil(regExpLib.htmlLoopTagEnd);
-    return obj;
-  },
-  text: (utility) => {
-    if (utility.prevElem && utility.prevElem.type == "text") {
-      utility.prevElem.text += utility.char.char;
-    } else {
-      return { type: "text", text: utility.char.char };
-    }
-  },
-};
-
-const parseAttribute = (utility) => {
-  if (!utility.char.char.match(regExpLib.attributeStart)) return;
-  utility.prevElem = undefined;
-  let attribute = utility.char.getString().match(regExpLib.attributeType)[0];
-  attribute = attribute.substring(0, attribute.length - 1);
-  utility.next(attribute.length - 1);
-  let obj = { attribute, value: "" };
-  let noValue = false;
-  utility.do((utility) => {
     utility.next();
-    if (utility.char.char.match(regExpLib.attributeValueExist)) {
-      noValue = true;
-      return true;
-    }
-    if (utility.char.char.match(regExpLib.attributeValueEqual)) return true;
-    return false;
-  });
-  if (noValue) {
-    utility.next(-1);
+    obj.value = value[0] ? value[0].text : "";
+    utility.prevElem = undefined;
     return obj;
-  }
-  utility.waitUntil(regExpLib.attributeValueStart, true, true);
-  let value = [];
-  utility.do((utility) => {
-    if (
-      utility.char.char.match(regExpLib.attributeValueEnd) &&
-      !utility.char.escaped
-    )
-      return true;
-    let result = step(utility, [parsers.html]);
-    if (result) {
-      value.push(result);
-      utility.prevElem = result;
+  };
+
+  parserOrder = [this.parsers.html, this.parsers.text];
+
+  step = (utility, ignore = []) => {
+    for (let i in this.parserOrder) {
+      if (ignore.includes(parserOrder[i])) continue;
+      let result = this.parserOrder[i](utility);
+      if (result || result === false) return result;
     }
-    utility.next();
-  });
-  utility.next();
-  obj.value = value[0] ? value[0].text : "";
-  utility.prevElem = undefined;
-  return obj;
-};
-
-export const parserOrder = [parsers.html, parsers.text];
-
-const step = (utility, ignore = []) => {
-  for (let i in parserOrder) {
-    if (ignore.includes(parserOrder[i])) continue;
-    let result = parserOrder[i](utility);
-    if (result || result === false) return result;
-  }
-};
+  };
+}
 
 class Utility {
   charArray;
@@ -207,7 +185,7 @@ class Utility {
   prevElem;
 
   constructor(string) {
-    this.charArray = genCharArray(string);
+    this.charArray = this.genCharArray(string);
     this.char = this.charArray[this.index];
   }
 
@@ -232,4 +210,37 @@ class Utility {
     while (!job(this)) {}
     last && this.next();
   };
+
+  genCharArray = (string) => {
+    let saveObj = {};
+    let escapedString = "";
+    let stringSplit = string.split("");
+    let charArray = [];
+    let escaped = false;
+    for (let i in stringSplit) {
+      if (stringSplit[i] == "\\" && !escaped) {
+        escaped = true;
+        continue;
+      }
+      let position = escapedString.length;
+      charArray.push({
+        char: stringSplit[i],
+        escaped,
+        position,
+        getString: function () {
+          return this.saveObj.string.substring(this.position);
+        }.bind({ saveObj, position }),
+      });
+      escapedString += stringSplit[i];
+      escaped = false;
+    }
+    saveObj.string = escapedString;
+    return charArray;
+  };
 }
+
+export const parserInstance = new Parser();
+
+export const parserOrder = parserInstance.parserOrder;
+export const parsers = parserInstance.parsers;
+export const parse = parserInstance.parse;
